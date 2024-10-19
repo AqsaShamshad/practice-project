72ab0b4eaf506cd591f4c66dc782a37082e1755a
Sohail <sohail@sodaclick.com>
2024-10-19 07:53:57 -0700

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-//Allow all requests from all domains & localhost
+// Allow all requests from all domains & localhost
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept");
@@ -11,33 +11,27 @@ app.all('/*', function(req, res, next) {
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 var ingredients = [
-    {
-        "id": "234kjw",
-        "text": "Eggs"
-    },
-    {
-        "id": "as82w",
-        "text": "Milk"
-    },
-    {
-        "id": "234sk1",
-        "text": "Bacon"
-    },
-    {
-        "id": "ppo3j3",
-        "text": "Frog Legs"
-    }
+    { "id": "234kjw", "text": "Eggs" },
+    { "id": "as82w", "text": "Milk" },
+    { "id": "234sk1", "text": "Bacon" },
+    { "id": "ppo3j3", "text": "Frog Legs" }
 ];
 
+// Home route
+app.get('/', function(req, res) {
+    res.send('Welcome to the Home Page! Here you can view or add ingredients.');
+});
 
+// Get ingredients
 app.get('/ingredients', function(req, res) {
     console.log("GET From SERVER");
     res.send(ingredients);
 });
 
+// Post new ingredient
 app.post('/ingredients', function(req, res) {
     var ingredient = req.body;
     console.log(req.body);
@@ -45,4 +39,7 @@ app.post('/ingredients', function(req, res) {
     res.status(200).send("Successfully posted ingredient");
 });
 
-app.listen(6069);
+// Start the server on port 6069
+app.listen(6069, function() {
+    console.log('Server running on port 6069');
+});
